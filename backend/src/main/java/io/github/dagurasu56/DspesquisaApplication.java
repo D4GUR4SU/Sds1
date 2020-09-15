ef306a0b3c28f31d24e3e2535c535c414a5a1104
Douglas Souza <douglas_search@hotmail.com>
2020-09-15 17:54:31 -0300

    Project created

---
+++ backend/src/main/java/io/github/dagurasu56/DspesquisaApplication.java
@@ -0,0 +1,13 @@
+package io.github.dagurasu56;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/dagurasu56/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dagurasu56;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

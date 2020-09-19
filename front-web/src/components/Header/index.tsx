d2cfc30edf846138fb84f4a7b9c45ace9111f4f7
Douglas Souza <douglas_search@hotmail.com>
2020-09-19 02:26:50 -0300

    Adicionando pagina home no React native

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const  Header = () => (
     <header className="main-header">
         <Logo />
         <Link to="/">
-            <div className="div-text">
+            <div className="logo-text">
                 <span className="logo-text-1">Big Game</span>
                 <span className="logo-text-2"> Survey</span>
             </div>
